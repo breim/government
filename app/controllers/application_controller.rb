1f14a266c349bb163bedcf0ef4de04389c9dc22a
Henrique Breim <henrique@breim.com.br>
2015-07-13 04:30:50 -0300

    Add support do custom fields on devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+private
+  before_filter :configure_devise_params, if: :devise_controller?
+  def configure_devise_params
+  	devise_parameter_sanitizer.for(:sign_up) do |u|
+  		u.permit(:name, :email, :password, :password_confirmation, :image, :image_file_name, :image_content_type, :image_file_size)
+  	end
+  	devise_parameter_sanitizer.for(:account_update) do |u|
+  		u.permit(:name, :email, :password, :password_confirmation, :image, :image_file_name, :image_content_type, :image_file_size)
+  	end
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+  	root_path
+  end
+
+
+
+
 end
