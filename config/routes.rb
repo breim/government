e50ea7e02a67f7cae9d59af48cf0edd93bdd9ca7
Henrique Breim <henrique@breim.com.br>
2015-07-13 23:01:54 -0300

    Add admins namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :questions
   root 'pages#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+
+  get '/backoffice' => 'admin/admin#index', as: :admin 
+  namespace :admin do
+  end
+
 end
