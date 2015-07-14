883fdfccef5e44117c920a2814232c93fadb351a
Henrique Breim <henrique@breim.com.br>
2015-07-13 23:03:37 -0300

    Add questions to admins namespace

---
+++ app/views/admin/questions/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @question, :id, :name, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get '/backoffice' => 'admin/admin#index', as: :admin 
   namespace :admin do
+  	resources :questions
   end
 
 end
