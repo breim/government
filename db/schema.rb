d95ed3c520bc0f00e04ee420b0fd1e810eb9562e
Henrique Breim <henrique@breim.com.br>
2015-07-13 22:44:23 -0300

    Add comments count fields for cache

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714014209) do
+ActiveRecord::Schema.define(version: 20150714014308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(version: 20150714014209) do
   create_table "questions", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "votes_count", default: 0
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "votes_count",    default: 0
+    t.integer  "comments_count", default: 0
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
